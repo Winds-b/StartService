2733bb4a2e9599746e355f30d30012bd48a2309e
Winds-b <1577910536@qq.com>
2016-09-08 21:23:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a1/startservice/MyService.java
@@ -0,0 +1,49 @@
+package com.example.a1.startservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by 1 on 2016/9/8.
+ */
+
+public class MyService extends Service {
+    private static final String TAG = "myServiceTag";
+
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        Log.v(TAG, "onCreate()");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.v(TAG, "onStartCommand()");
+        int n = intent.getIntExtra("num", 0);
+        Log.v(TAG, "random int is:" + n);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v(TAG, "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v(TAG, "onBind()");
+        return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.v(TAG, "onUnbind()");
+        return super.onUnbind(intent);
+    }
+}
